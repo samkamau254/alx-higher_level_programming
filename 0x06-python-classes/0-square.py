cb89b48e9bf3aa9f9e46e461a634a155db3b4f81
samkamau254 <samkamau254@outlook.com>
2023-12-19 08:00:20 -0500

    lib

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is a module called 0-square. """
 class Square:
-    """Class Square that defines a square. """
+    """Class Square that defines a square.
+    """
     pass
