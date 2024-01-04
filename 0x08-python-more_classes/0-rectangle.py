a1d1ad36e76263378d3021ddf8f770d7113142f9
samkamau254 <samkamau254@outlook.com>
2024-01-04 05:29:46 -0500

    OOP

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module is called 0-rectangle.
-    It defines a rectangle.
+    defines a rectangle.
 """
 
 
 class Rectangle:
     """The class Rectangle defines a rectangle
-    and does nothing.
+    with no instances and attributes.
     """
     pass
